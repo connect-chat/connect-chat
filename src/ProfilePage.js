f6aaaf42a44535565af754ad2c27cc805d808417
Niki Hite <nikihite@comcast.net>
2022-07-20 18:13:46 -0700

    got info from API onto profilePage

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -15,10 +15,16 @@ export default function ProfilePage() {
     userDetails();
   }, []);
   return ( 
-    <p>{ sign }</p>
-    // sign.map((zodiac, i) => <div
-    //   key={zodiac.sign + i} className='zodiac-info'>
-    //   <p>{zodiac.current_date}</p>
-    // </div>)
+    <div>
+      <p>Your Sign: { sign }</p>
+      <p>Todays Date: { horoscope.current_date }</p>
+      <p>Horoscope Compatibility: { horoscope.compatibility }</p>
+      <p>Todays Lucky Number: { horoscope.lucky_number }</p>
+      <p>Todays Lucky Time: { horoscope.lucky_time }</p>
+      <p>Color: { horoscope.color }</p>
+      <p>{ sign }s are born { horoscope.date_range }</p>
+      <p>You could be feeling: { horoscope.mood }</p>
+      <p>{ horoscope.description }</p>
+    </div>
   );
 }
